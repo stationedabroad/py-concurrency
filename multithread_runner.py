978f845f207cc0d8ab41d4bc94e43099b93e223b
Sulman <sulmanmh@gmail.com>
2020-04-28 23:10:43 +0100

    multi-thread impl added

---
+++ multithread_runner.py
@@ -0,0 +1,22 @@
+import time
+import threading
+from serial_random import fn
+
+if __name__ == '__main__':
+	st = time.time()
+	size = 1000000
+	threads = 10
+	jobs = []
+	for i in range(0, threads):
+		l = list()
+		thread = threading.Thread(target=fn(size, l))
+		jobs.append(thread)
+	
+	for j in jobs:
+		j.start()
+	for j in jobs:
+		j.join()
+
+	print('Multi-thread processing finished ...')
+	elapsed = time.time() - st
+	print(f'multi-thread processing = {elapsed}')
--- serial_runner.py
+++ serial_runner.py
@@ -1,4 +1,4 @@
-from serial_runner import fn
+from serial_random import fn
 import time
 
 if __name__ == '__main__':
