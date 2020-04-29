fa46f7e1512c8be4c4482c44b8c9c9844d47845f
Sulman <sulmanmh@gmail.com>
2020-04-29 23:12:21 +0100

    updated serial func to output test calls

--- serial_runner.py
+++ serial_runner.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     
     for i in range(runs):
         l = list()
-        fn(size, l)
+        fn(size, l, i)
 
     print('Serial processing finished ...')
     elapsed = time.time() - st
