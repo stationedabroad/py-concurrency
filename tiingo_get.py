2d0fd72ed39d2c1232e051b3155aabea269f6bfb
Sulman <sulmanmh@gmail.com>
2020-05-20 19:39:26 +0100

    modified tiingo api get with alternate single arg func

--- tiingo_get.py
+++ tiingo_get.py
@@ -28,3 +28,28 @@ def get_eod_price(stock, caller_id):
         raise Exception(f'API error {url}')
     
     print(f'Call - {caller_id} completed for ticker: {stock} with status: {status}')
+
+
+def _get_eod_price(stock):
+
+    header = {
+        'Content-Type': 'application/json'
+    }    
+
+    token = os.getenv("TIINGO_TK")
+    url = "https://api.tiingo.com/tiingo/daily/{ticker}/prices?token={token}"
+    
+
+    try:
+        url = url.format(ticker=stock, token=token)
+        st = time()
+        content = requests.get(url, headers=header)
+        status = content.status_code
+        logger.info(f'Tiingo APi call for {stock} completed in: {time() - st}')
+    except Exception as e:
+        status = None
+        print(f"exception during API call - {e}")
+        logger.error(f"Exception during SPI call for: {stock}, error: {e}")
+        raise Exception(f'API error {url}')
+    
+    print(f'Completed for ticker: {stock} with status: {status}')
