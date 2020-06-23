2145866d923bccf5ff69c61ca8e1644793004c22
Sulman <sulmanmh@gmail.com>
2020-06-23 22:18:33 +0100

    async tiingo get func

--- tiingo_get.py
+++ tiingo_get.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import requests
 from time import time
@@ -53,3 +54,28 @@ def _get_eod_price(stock):
         raise Exception(f'API error {url}')
     
     print(f'Completed for ticker: {stock} with status: {status}')
+
+
+async def get_eod_price_async(stock, caller_id):
+
+    header = {
+        'Content-Type': 'application/json'
+    }    
+
+    token = os.getenv("TIINGO_TK")
+    url = "https://api.tiingo.com/tiingo/daily/{ticker}/prices?token={token}"
+    
+
+    try:
+        url = url.format(ticker=stock, token=token)
+        st = time()
+        content = await requests.get(url, headers=header)
+        status = content.status_code
+        logger.info(f'Tiingo APi call for {stock} completed in: {time() - st}')
+    except Exception as e:
+        status = None
+        print(f"exception during API call - {e}")
+        logger.error(f"Exception during SPI call for: {stock}, error: {e}")
+        raise Exception(f'API error {url}')
+    
+    print(f'Call - {caller_id} completed for ticker: {stock} with status: {status}')    
